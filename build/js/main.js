732843acb874ef28f6bd5451d6d79822f29e19e8
www512 <www512@yandex.ru>
2019-12-09 23:01:49 +0300

    Fitness Center

--- build/js/main.js
+++ build/js/main.js
@@ -2,30 +2,6 @@
 
 const esc = 27;
 
-//Scroll
-const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-      animationTime = 400,
-      framesCount = 30;
-
-anchors.forEach(function(item) {
-  item.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-
-    let scroller = setInterval(function() {
-      let scrollBy = coordY / framesCount;
-
-      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        window.scrollBy(0, scrollBy);
-      } else {
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-    }, animationTime / framesCount);
-  });
-});
-
 //Mask in a modal window on a phone number
 let phone = document.getElementById('phone');
 
